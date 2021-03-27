708f6cde0b8089a52e45ba2d67b7438104e11e0a
Ryan Whited <rwhited@gobasports.com>
2021-03-27 08:09:03 -0500

    Adding more images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,6 @@ suspensionDF <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFacto
 total_summary <- suspensionDF %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD = sd(PSI) , .groups = 'keep') 
 
 lot_summary <- suspensionDF %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD = sd(PSI) , .groups = 'keep') 
+
+#Deliverable 3
+
