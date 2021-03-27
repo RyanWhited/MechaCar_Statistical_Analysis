45d14ae565a3496e619bc2f9c2b6812224025a1b
Ryan Whited <rwhited@gobasports.com>
2021-03-26 20:21:39 -0500

    Updated files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,6 +2,6 @@ library(tidyverse)
 
 mechaCarDF <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
-lm(vehicle_length ~ mpg, mechaCarDF) 
+lm(vehicle_length + vehicle_weight + spoiler_angle + groud_clearance + AWD + mpg, data=mechaCarDF) 
 
 summary(lm(vehicle_length ~ mpg, mechaCarDF))
