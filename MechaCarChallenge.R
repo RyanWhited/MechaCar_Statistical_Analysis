aab0fa04f20ea46fdee2bf71f6d3247311f0096d
Ryan Whited <rwhited@gobasports.com>
2021-03-26 21:18:45 -0500

    Updated Rscript and images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,17 @@
+# Deliverable 1
+
 library(tidyverse)
 
 mechaCarDF <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
-lm(vehicle_length + vehicle_weight + spoiler_angle + groud_clearance + AWD + mpg, data=mechaCarDF) 
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCarDF) 
 
-summary(lm(vehicle_length ~ mpg, mechaCarDF))
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCarDF))
+
+#Deliverable 2
+
+suspensionDF <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- suspensionDF %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD = sd(PSI) , .groups = 'keep') 
+
+lot_summary <- suspensionDF %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD = sd(PSI) , .groups = 'keep') 
